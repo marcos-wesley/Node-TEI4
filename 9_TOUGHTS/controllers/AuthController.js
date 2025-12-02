221f1cec7fa9eb194af6fc0e494eddb367fa6e40
marcos-wesley <marcos.wesley@hotmail.com.br>
2025-12-01 21:28:51 -0300

    Aula 01/12 - 2

--- 9_TOUGHTS/controllers/AuthController.js
+++ 9_TOUGHTS/controllers/AuthController.js
@@ -1,3 +1,4 @@
+const { where } = require('sequelize')
 const User = require('../models/User')
 
 const bcrypt = require('bcryptjs')
@@ -6,6 +7,29 @@ module.exports = class AuthController {
     
     static login(req, res) {
         res.render('auth/login')
+    }
+
+    static async loginPost(req, res) {
+        
+        const {email, password} = req.body
+        
+        //find user 
+        const user = await User.findOne({where: {email: email}})
+
+        if(!user) {
+            req.flash('message', 'Usuário não encontrado!')
+            res.render('auth/login')
+        }
+
+        //check if passwords match
+        const passwordMatch = bcrypt.compareSync(password, user.password)
+
+        if(!passwordMatch) {
+            req.flash('message', 'Senha inválida!')
+            res.render('auth/login')
+
+            return
+        }
     }
 
     static register(req, res) {
@@ -22,6 +46,15 @@ module.exports = class AuthController {
 
             return
         }
+
+        //initialize session
+        req.session.userid = user.id
+
+        req.flash('message', 'Autenticação realizada com sucesso!')
+
+        req.session.save(() => {
+            res.redirect('/')
+        })
 
         //check if user exists
 
@@ -47,20 +80,17 @@ module.exports = class AuthController {
         }
 
         try {
-            await User.create(user)
+            const createdUser = await User.create(user)
 
-            //initialize session
-            req.session.userid = userid
-
-            req.flash('message', 'Cadastro realizado com sucesso')
-
-
-            req.session.save(() => {
-                res.redirect('/')
-            })
+            
         }catch(err) {
             console.log(err)
         }
+    }
+
+    static logout(req, res){ 
+        req.session.destroy()
+        res.redirect('/login')
     }
 
 }
